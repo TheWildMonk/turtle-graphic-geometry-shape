8c2d9371986c65da789b46a6a6e583fada8973d8
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-10 22:12:29 +0600

    Update geometry_shapes.py

--- geometry_shapes.py
+++ geometry_shapes.py
@@ -1,25 +1,26 @@
 from turtle import Turtle, Screen
 
+colors = ["LightSeaGreen", "DarkSlateBlue", "lawngreen", "HotPink4", "SpringGreen3", "tomato2", "OliveDrab4", "red4"]
+
 
 class Shape:
     def __init__(self, sides):
         self.sides = sides
 
-    def calculate_shape(self):
+    def calculate_angle(self):
         angle = 360/self.sides
         return angle
 
 
 def move(geometry_shape):
     for each_side in range(geometry_shape):
-        tim.right(Shape(geometry_shape).calculate_shape())
+        tim.right(Shape(geometry_shape).calculate_angle())
         tim.forward(100)
 
 
-colors = ["LightSeaGreen", "DarkSlateBlue", "lawngreen", "HotPink4", "SpringGreen3", "tomato2", "OliveDrab4"]
 tim = Turtle()
 tim.shape("turtle")
-for each_shape in range(3, 10):
+for each_shape in range(3, 11):
     tim.color(colors[each_shape-3])
     move(each_shape)
 
